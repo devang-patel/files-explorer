fe56c44be9a5e60107bf8adb18da6cca6539dcb1
Devang Patel <pateldevang10@gmail.com>
2013-12-16 00:48:21 +0530

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var destApp = angular.module('filesExplorer', ['ui.bootstrap']);
+
+// Declare app level module which depends on filters, and services
+destApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/view1', {templateUrl: 'partial/1', controller: MyCtrl1});
+    $routeProvider.when('/view2', {templateUrl: 'partial/2', controller: MyCtrl2});
+    $routeProvider.when('/view', {templateUrl: 'partial/0', controller: MyCtrl3});
+    $routeProvider.otherwise({redirectTo: '/view'});
+    $locationProvider.html5Mode(true);
+  }]);
+
+destApp.controller('DestinationController', function ($scope, $http, $filter) {
+
+    $http.get('data/server-data.json').success(function(data) {
+        
+        $scope.filters = { };
+        $scope.destinations = data;
+        $scope.filteredDestinations = [];
+        $scope.pageSize = 10;
+        $scope.maxSize = 10;
+   
+        $scope.FilterType = function(text) {
+            $scope.filteredDestinations = $filter('filter')($scope.destinations, text);
+        };
+
+        $scope.$watch('cityName', function (newCityName) {
+            $scope.currentPage = 1;
+            $scope.filteredDestinations = $filter('filter')($scope.destinations, $scope.cityName);
+            $scope.noOfPages = Math.ceil($scope.filteredDestinations.length/$scope.pageSize); 
+        });
+    });
+    
+});
+
+destApp.filter('startFrom', function () {
+    return function (input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    };
+});
---
+++ public/js/controllers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* Controllers */
+
+
+
+function MyCtrl1() {}
+MyCtrl1.$inject = [];
+
+
+function MyCtrl2() {
+}
+MyCtrl2.$inject = [];
+
+function MyCtrl3() {
+}
+MyCtrl3.$inject = [];
+
+
